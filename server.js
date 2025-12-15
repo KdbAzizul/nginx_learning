f3cd50321a419764fddb8cfdaf0020c92f3cdc6a
KdbAzizul <kdbazizul@gmail.com>
2025-12-15 19:18:44 +0600

    checking Load balancing

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express'); // Import the express module
 const app = express(); // Create an Express application instance
 const port = 3000; // Define the port number
 
+
+const appId = process.env.APP_ID || 'Unknown Instance';
 // Define a route for GET requests to the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World with NGINX!'); // Send a response
+  res.send(`Hello World from ${appId}!`); // Send a unique response
 });
 
 // Start the server and listen for incoming connections
